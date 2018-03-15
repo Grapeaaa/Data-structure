fb576e94335f819e53969cc9085c48dc5151decf
lc <lccmuc@gmail.com>
2018-03-15 10:49:28 +0800

    修改格式

--- 线性结构/合并有序链表.cpp
+++ 线性结构/合并有序链表.cpp
@@ -1,6 +1,6 @@
 List Merge( List L1, List L2)
 {
-	List tmp,head;
+	List tmp,head;//建立头节点 
 	tmp = (List) malloc(sizeof(struct Node));
 	List p = L1->Next;
     List q = L2->Next;
@@ -21,7 +21,7 @@ List Merge( List L1, List L2)
 	tmp->Next = NULL;
 	if(p)  tmp->Next = p;
 	if(q)  tmp->Next = q;
-	L1->Next=NULL;
+	L1->Next=NULL;//释放内存 
     L2->Next=NULL;
 	return head;
  } 
